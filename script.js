9cd78d965085f7c7855684806da666425c6e2fcc
Omerhagoat <gameomerr@gmail.com>
2026-06-29 13:55:14 +0300

    Refactor script.js to create grid for game
    Removed getInput function and added grid creation logic.

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-function getInput(){
-    let UserInput = prompt("enter number ");
-    let squared = UserInput*UserInput;
-        if(squared%2==0){
-            document.writeln("<div class =text>" +squared+"</div>");
-        }
-    alert("Done");
+
+const grid=document.getElemendById('grid');
+let squares = [];
+let currentSnake = [0,1,2];
+let direction = 1;
+let appleIndex = 0;
+let score = 0;
+let timerId = 0;
+let intervalTime = 200;
+function createGrid(){
+            
+    for(let i=0; i<400; i++){
+        const square = document.createElement('div');
+        grid.appendChild(square);
+        squares.push(square);
+    }
 }
 
-getInput()
+
+createGrid();   
