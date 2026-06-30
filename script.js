150d3264c3ec2283605e6ea7bc8a524e3f273691
Omerhagoat <gameomerr@gmail.com>
2026-06-30 10:28:45 +0300

    Refactor grid creation and game logic functions

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-
 const grid=document.getElemendById('grid');
 let squares = [];
-let currentSnake = [0,1,2];
+let currentSnake = [2, 1, 0];
 let direction = 1;
 let appleIndex = 0;
 let score = 0;
 let timerId = 0;
 let intervalTime = 200;
-function createGrid(){
-            
+function createBoard(){
     for(let i=0; i<400; i++){
         const square = document.createElement('div');
         grid.appendChild(square);
@@ -17,4 +15,43 @@ function createGrid(){
 }
 
 
-createGrid();   
+createBoard();   
+
+function startGame() {
+    currentSnake.forEach(index => squares[index].classList.remove('snake'));
+    squares[appleIndex].classList.remove('apple');
+    clearInterval(timerId);
+}
+
+function move(){
+
+    const hitBottom=(currentSnake[0]+20>=400 && direction === 20);
+    const hitTop = (currentSnake[0]-20<0 && direction === -20);
+    const hitRight = (currentSnake[0]%20 === 19 && direction === 1)
+    const hitLeft = (currentSnake[0]%20 === 0 && direction === -1)
+    const hitSelf = squares[currentSnake[0]+direction]?.classList.contains('snake');
+}
+
+function generateApple(){
+    do{
+        appleIndex = Math.floor(Math.random()* squares.length);
+    } while (squares[appleIndex].classList.contains('snake'));
+    squares[appleIndex].classList.add('apple');
+}
+
+function changeDir(newDir) {
+    if (direction + newDir !== 0) {
+        direction = newDir;
+    }
+}
+
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'ArrowUp') changeDir(-20);
+    if (e.key === 'ArrowDown') changeDir (20);
+    if (e.key === 'ArrowLeft') changeDir (-1);
+    if (e.key === 'ArrowRight') changeDir (-1); 
+    
+});
+
+
